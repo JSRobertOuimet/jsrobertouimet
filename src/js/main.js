612b8d14369533eef6b44ccf1f933109494eb9a5
Jean-Simon Robert Ouimet <jr@smbsol.com>
2017-02-08 11:55:41 -0500

    + links update; nav links & pages

--- src/js/main.js
+++ src/js/main.js
@@ -7,3 +7,50 @@ function openNav() {
 function closeNav() {
   document.getElementById("nav").style.width = "0%";
 }
+
+// var mus = document.getElementById("mus");
+// var isPlaying = false;
+
+// function togglePlay() {
+//   if (isPlaying) {
+//     mus.pause()
+//   } else {
+//     mus.play();
+//   }
+// };
+// mus.onplaying = function() {
+//   isPlaying = true;
+// };
+// mus.onpause = function() {
+//   isPlaying = false;
+// };
+
+var playing = false;
+var initDone = false;
+
+var audioElement = null;
+
+$("#music").click(function() {
+  if (playing) {
+    // Stop playing
+    audioElement.pause();
+  } else {
+    // Start playing
+    if (!initDone) {
+      initDone = true;
+
+      audioElement = document.createElement('audio');
+      audioElement.setAttribute('src', 'snd/test.mp3');
+
+      audioElement.addEventListener('ended', function() {
+        this.currentTime = 0;
+        this.play();
+      }, false);
+    }
+
+    audioElement.play();
+  }
+
+  $(this).find('i').toggleClass('fa fa-play-circle fa fa-pause-circle');
+  playing = !playing;
+});
